cc1cb6dd691772fcd549823bd347cd31789750df
Dylan Werner-Meier <dylan.bloger@gmail.com>
2020-06-15 02:14:34 +0200

    add accumulo conf & proxy

--- accumulo-base-image/entrypoint.sh
+++ accumulo-base-image/entrypoint.sh
@@ -7,4 +7,14 @@ configureXml CORE_SITE /usr/local/hadoop/etc/hadoop/core-site.xml
 
 configureXml ACCUMULO_SITE /usr/local/accumulo/conf/accumulo-site.xml
 
+if [ ! -z "$ACCUMULO_SITE_instance_zookeeper_host" ]; then
+	echo "[-] Forwarding instance.zookeeper.host to zookeepers=${ACCUMULO_SITE_instance_zookeeper_host}"
+	sed -i "s/zookeepers=.*/zookeepers=${ACCUMULO_SITE_instance_zookeeper_host}/" proxy/proxy.properties
+fi
+
+if [ ! -z "$PROXY_instance_name" ]; then
+	echo "[-] Setting instance=${PROXY_instance_name} in proxy/proxy.properties"
+	sed -i "s/instance=.*/instance=${PROXY_instance_name}/" proxy/proxy.properties
+fi
+
 exec "$@"
